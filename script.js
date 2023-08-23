c0fb59b8d7366e7fa317aa2e065cfd21d32a2f00
Khaled Abdulhamid <khaledraedabdulhamed101@hotmail.com>
2023-08-23 13:07:41 -0400

    first push

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const googleDatabase = [
+    'cats.com',
+    'souprecipes.com',
+    'flowers.com',
+    'animals.com',
+    'facebook.com',
+    'bms.com'
+];
+
+
+const googleSearch = (searchInput, db) => {
+    const matches = db.filter(website => {
+        return website.includes(searchInput)
+    })
+
+    return matches.length > 3 ? matches.slice(0,3) : matches;
+}
+
+console.log(googleSearch('cat', googleDatabase));
+
+module.exports= googleSearch;
